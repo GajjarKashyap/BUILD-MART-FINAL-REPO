c364a983e861936f79f5aa02bc2c0095c443c58a
Kashayap Gajjar <kashayapgajjar71@gmail.com>
2026-07-21 01:18:21 +0530

    Extract Amazon child variations

--- buildmart-amazon-importer/amazon-content.js
+++ buildmart-amazon-importer/amazon-content.js
@@ -1,12 +1,9 @@
 // Amazon page message bridge.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'EXTRACT') {
-    try {
-      const data = extractProductData();
-      sendResponse({ success: true, data });
-    } catch (err) {
-      sendResponse({ success: false, error: err.message });
-    }
+    Promise.resolve(extractProductData())
+      .then(data => sendResponse({ success: true, data }))
+      .catch(err => sendResponse({ success: false, error: err.message }));
     return true;
   }
 });
